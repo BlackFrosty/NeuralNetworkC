f6850e91c4970a0b8f7554cab917ad8d2e97bf3a
tmdt2021 <tm.dt@laposte.net>
2021-06-21 12:29:26 +0200

    Correction caractères accentués

--- src/Bitmap.h
+++ src/Bitmap.h
@@ -2,34 +2,34 @@
 #define BITMAP_H_INCLUDED
 
 
-//nombre maximum de lignes à lire dans le fichier d'apprentissage
+//nombre maximum de lignes Ã  lire dans le fichier d'apprentissage
 #define NB_ENTREES_APPR     60000
 
-//nombre maximum de lignes à lire dans le fichier de test
+//nombre maximum de lignes Ã  lire dans le fichier de test
 #define NB_ENTREES_TEST     10000
 
-#define NB_CHAMPS           4 //nombre de champs de la structure de données (voir si toujours utile)    
+#define NB_CHAMPS           4 //nombre de champs de la structure de donnÃ©es (voir si toujours utile)
 
 //nombre de lignes et de colonnes d'un fichier bitmap
 #define NB_LIGNES_BMP       28
 #define NB_COLONNES_BMP     28
 
-//taille en octet d'un bloc à lire dans les fichiers d'entrée
+//taille en octet d'un bloc ï¿½ lire dans les fichiers d'entrÃ©e
 #define TAILLE_BLOC         4
 
 #define CLE_MAGIQUE_IMG    2051
 #define CLE_MAGIQUE_TST     2049
 
 
-//définition des constantes de classification d'éléments
+//dÃ©finition des constantes de classification d'Ã©lÃ©ments
 typedef enum {
     ZERO = 0, UN = 1, DEUX = 2, TROIS = 3, QUATRE = 4
     , CINQ = 5, SIX = 6, SEPT = 7, HUIT = 8, NEUF = 9
     , INCONNU = -1
 } T_LABEL;//T_LABEL_CHIFFRES;
 
-//commenter le précédent et décommenter le suivant
-//pour traiter des vêtements au lieu des chiffres
+//commenter le prÃ©cÃ©dent et dÃ©commenter le suivant
+//pour traiter des vÃ©tements au lieu des chiffres
 /*
 typedef enum {
     TSHIRT_TOP = 0, TROUSER = 1, PULLOVER = 2, DRESS = 3, COAT = 4
@@ -39,7 +39,7 @@ typedef enum {
 */
 
 
-//définition du type structuré T_BITMAP
+//dÃ©finition du type structurÃ© T_BITMAP
 typedef struct {
 
     //char car besoin d'un seul octet par bit
@@ -51,24 +51,24 @@ typedef struct {
 
 
 
-//fonction qui lit un ensemble d'éléments à partir d'un fichier d'entrées
-//et les stocke dans un tableau. Renvoi le nombre d'éléments lus.
+//fonction qui lit un ensemble d'Ã©lÃ©ments Ã  partir d'un fichier d'entrÃ©es
+//et les stocke dans un tableau. Renvoi le nombre d'Ã©lÃ©ments lus.
 long lireEntrees(char* ficEntrees, T_BITMAP* tabEntrees);
 
-//fonction d'affichage d'un élément contenu dans un tableau
-//Après propagation avant par le PMC, la probabilité de chaque
-//reconnaissance est mis à jour dans ce tableau.
-//Cette probabilité est affichable avec le booleen afficherProba)
+//fonction d'affichage d'un Ã©lÃ©ment contenu dans un tableau
+//AprÃ¨s propagation avant par le PMC, la probabilitÃ© de chaque
+//reconnaissance est mis Ã  jour dans ce tableau.
+//Cette probabilitÃ© est affichable avec le booleen afficherProba)
 void afficherEntree(T_BITMAP *elem, char afficherProba, T_LABEL label);
 
-//fonction d'affichage d'un tableau d'entrées
+//fonction d'affichage d'un tableau d'entrÃ©es
 void afficherTabEntrees(T_BITMAP *tabEntrees, long nbEntrees
     , T_LABEL label, char afficherProba);
 
-//renvoie le label associé un numéro de neurone de sortie
+//renvoie le label associÃ© un numÃ©ro de neurone de sortie
 T_LABEL getLabel(long numNeurSortie);
 
-//retourne une chaine caractère associée à un label
+//retourne une chaine caractÃ¨re associÃ©e Ã  un label
 const char* getStrLabel(T_LABEL label);
 
 //affichage d'une image pixel par pixel
