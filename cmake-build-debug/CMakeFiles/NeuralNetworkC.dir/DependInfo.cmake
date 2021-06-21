bf059bead755e0afbcc5c20eef9b4fd4f31c5296
tmdt2021 <tm.dt@laposte.net>
2021-06-21 12:52:12 +0200

    Modif Makefile, ajout CMake

---
+++ cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/user/prog/NeuralNetworkC/src/Bitmap.c" "/home/user/prog/NeuralNetworkC/cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/src/Bitmap.c.o"
+  "/home/user/prog/NeuralNetworkC/src/Couche.c" "/home/user/prog/NeuralNetworkC/cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/src/Couche.c.o"
+  "/home/user/prog/NeuralNetworkC/src/Iris.c" "/home/user/prog/NeuralNetworkC/cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/src/Iris.c.o"
+  "/home/user/prog/NeuralNetworkC/src/Neurone.c" "/home/user/prog/NeuralNetworkC/cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/src/Neurone.c.o"
+  "/home/user/prog/NeuralNetworkC/src/PMC.c" "/home/user/prog/NeuralNetworkC/cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/src/PMC.c.o"
+  "/home/user/prog/NeuralNetworkC/src/Statistiques.c" "/home/user/prog/NeuralNetworkC/cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/src/Statistiques.c.o"
+  "/home/user/prog/NeuralNetworkC/src/Util.c" "/home/user/prog/NeuralNetworkC/cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/src/Util.c.o"
+  "/home/user/prog/NeuralNetworkC/src/file_mgmt.c" "/home/user/prog/NeuralNetworkC/cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/src/file_mgmt.c.o"
+  "/home/user/prog/NeuralNetworkC/src/main.c" "/home/user/prog/NeuralNetworkC/cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/NeuralNetworkC.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NeuralNetworkC.dir/src/Bitmap.c.o"
+  "CMakeFiles/NeuralNetworkC.dir/src/Couche.c.o"
+  "CMakeFiles/NeuralNetworkC.dir/src/Iris.c.o"
+  "CMakeFiles/NeuralNetworkC.dir/src/Neurone.c.o"
+  "CMakeFiles/NeuralNetworkC.dir/src/PMC.c.o"
+  "CMakeFiles/NeuralNetworkC.dir/src/Statistiques.c.o"
+  "CMakeFiles/NeuralNetworkC.dir/src/Util.c.o"
+  "CMakeFiles/NeuralNetworkC.dir/src/file_mgmt.c.o"
+  "CMakeFiles/NeuralNetworkC.dir/src/main.c.o"
+  "NeuralNetworkC"
+  "NeuralNetworkC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NeuralNetworkC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
