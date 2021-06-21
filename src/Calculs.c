aaae7905ca3576f62814c7a3074d6a96d7807516
Maskott <themaskott@gmail.com>
2021-06-21 16:35:10 +0200

    ajout fichiers Calculs

---
+++ src/Calculs.c
@@ -0,0 +1,76 @@
+//
+//  Calculs.c
+//  Calculs
+//
+//  Created by Maskott on 21/06/2021.
+//
+
+/*---------------------------------------------*/
+/*             INCLUDE LANGAGE C               */
+/*---------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+/*---------------------------------------------*/
+/*     INCLUDE PERSONNELS   - prototypes       */
+/*---------------------------------------------*/
+
+#include "Calculs.h"
+#include "Structures.h"
+
+
+/*-----------------------------------------------------------------*/
+/*  sigmoide                                                       */
+/* Fonction : calcule la sigmoide de x                             */
+/* Entree :  un reel x                                             */
+/* Sortie :  Le resultat sigmoide(x)                               */
+/*-----------------------------------------------------------------*/
+
+double sigmoide ( double x ){
+   return ( 1.0 / ( 1.0 + exp( -x ) ) ) ;
+}
+
+/*-----------------------------------------------------------------*/
+/*  derivSigmoide                                                  */
+/* Fonction : calcule la valeur de la derivee de la sigmoide en x  */
+/* Entree :  un reel la valeur de sigmoide en x                    */
+/* Sortie :  Le resultat sigmoide'(x)                              */
+/* Attention : sigmoide'(x) = sigmoide(x)*(1 - sigmoide(x) )       */
+/* il faut calculer avant sigmoide(x)                              */
+/*-----------------------------------------------------------------*/
+
+double derivSigmoide( double dSigmox ){
+    return dSigmox * ( 1 - dSigmox ) ;
+}
+
+/*--------------------------------------------------------------------------------------------------------------*/
+/*  sommeExpProba                                                                                               */
+/* Fonction : calcule la somme des exponentielle des valeurs de la couche de sortie qui sera passee a softmax() */
+/* Entree :  un pointeur vers la couche de sortie et sa taille                                                  */
+/* Sortie :  un double qui sera utilise par softmax                                                             */
+/*--------------------------------------------------------------------------------------------------------------*/
+
+double sommeExpProba( T_COUCHE * coucheSortie ){
+    double dSommeExp = 0 ;
+    for ( int i = 0 ; i < coucheSortie->ui16NbNeurones ; i++ ){
+        dSommeExp += exp( (coucheSortie->pNeur[i]).dValeurSortie ) ;
+    }
+    return dSommeExp ;
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  softmax                                                                                                */
+/* Fonction : remplace la valeur de sortie d un neurone de la couche sortie par exp(val)/somme(exp(val_i)) */
+/* Entree :  pointeur vers la couche de sortie, la valeur sommeExpProba et le neuronne d'application       */
+/* Sortie :  un double = nouvelle valeur de sortie du neurone                                              */
+/*---------------------------------------------------------------------------------------------------------*/
+
+void softmax( T_COUCHE * coucheSortie, double dSeommeExp ){
+    for ( int i = 0 ; i < coucheSortie->ui16NbNeurones ; i ++ ){
+        (coucheSortie->pNeur[i]).dValeurSortie = exp( (coucheSortie->pNeur[i]).dValeurSortie ) / dSeommeExp ;
+    }
+}
+
---
+++ src/Calculs.h
@@ -0,0 +1,18 @@
+//
+//  Calculs.h
+//  Calculs
+//
+//  Created by Maskott on 21/06/2021.
+//
+
+#ifndef Calculs_h
+#define Calculs_h
+
+#include <stdio.h>
+
+double sigmoide ( double ) ;
+double derivSigmoide( double ) ;
+double sommeExpProba( T_COUCHE * ) ;
+void softmax( T_COUCHE *, double ) ;
+
+#endif /* Calculs_h */
