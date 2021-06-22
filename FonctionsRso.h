6f09b922cb891e71cd533502daf2cad79668aa78
Maskott <themaskott@gmail.com>
2021-06-22 14:24:26 +0200

    fonction de propagation

---
+++ FonctionsRso.h
@@ -0,0 +1,18 @@
+//
+//  FonctionsRso.h
+//  calculs
+//
+//  Created by Maskott on 22/06/2021.
+//
+
+#ifndef FonctionsRso_h
+#define FonctionsRso_h
+
+#include <stdio.h>
+
+
+void propager( T_RSO * , double [] ) ;
+void calculerProbaCoucheCahee( T_COUCHE *, double [] ) ;
+void calculerProbaCoucheSortie( T_COUCHE *, T_COUCHE * ) ;
+
+#endif /* FonctionsRso_h */
